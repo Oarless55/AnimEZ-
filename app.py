f070f040f9ecac476463979b14d83e4855614981
Oarless <24130776@stu.omu.edu.tr>
2025-11-15 05:34:13 +0300

    still trying

--- app.py
+++ app.py
@@ -341,15 +341,8 @@ def get_anime_detail(slug):
     })
 
 if __name__ == '__main__':
-    import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(
-        debug=True,
-        host='0.0.0.0',
-        port=port,
-        threaded=True
-    )
-  
+    app.run(debug=True, port=5000, host='0.0.0.0')
+
 
 
 
