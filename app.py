4448041013a08156a076a4517c4ac271f7751b22
Oarless <24130776@stu.omu.edu.tr>
2025-11-15 05:28:31 +0300

    Deneme

--- app.py
+++ app.py
@@ -341,8 +341,16 @@ def get_anime_detail(slug):
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(
+        debug=True,
+        host='0.0.0.0',
+        port=port,
+        threaded=True
+    )
   
+
 
 
 
